53419b24d4740c4adeb2621706757b44bbfa3087
Lucas Borges <diasb.trabalho@gmail.com>
2022-05-02 21:15:13 -0300

    Listagem de Galpões

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -14,7 +14,6 @@ describe "Usuario visita a tela inicial" do
 	it "e vê a lista os galpões" do
 		# Arrange
 		Warehouse.create(name:"Rio", code:"SDU",city:"Rio de Janeiro",area:60_000)
-		Warehouse.create(name:"Maceio", code:"MCZ",city:"Maceio",area:50_000)
 
 		# Act
 		visit("/")
@@ -24,10 +23,5 @@ describe "Usuario visita a tela inicial" do
 		expect(page).to have_content("Código: SDU")
 		expect(page).to have_content("Cidade: Rio de Janeiro")
 		expect(page).to have_content("60000 m²")
-
-		expect(page).to have_content("Maceio")
-		expect(page).to have_content("Código: MCZ")
-		expect(page).to have_content("Cidade: Maceio")
-		expect(page).to have_content("50000 m²")
 	end
 end
